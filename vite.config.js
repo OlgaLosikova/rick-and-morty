fbf14886a9abdbe795b3ba426cab48b736b8322b
OlgaLosikova <techtigra@gmail.com>
2024-06-10 11:04:52 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/rick-and-morty/',
   plugins: [vue()],
 })
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: "/rick-and-morty",
+  publicPath: "/rick-and-morty/",
 };
